7d4d0ae6aff292f8256b2d76086b4c53feb3532c
donglu <donglu@cs.sh.cn>
2018-06-07 16:55:03 +0800

    订单系统初始化

---
+++ order-jdbc/src/main/java/com/three/order/orderjdbc/OrderJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.three.order.orderjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderJdbcApplication.class, args);
+	}
+}
---
+++ order-rest/src/main/java/com/three/order/orderrest/OrderRestApplication.java
@@ -0,0 +1,12 @@
+package com.three.order.orderrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderRestApplication.class, args);
+	}
+}
