dd39de2c13e6d62c7c917b0c9688402bbe3c7358
s <945142626@qq.com>
2018-07-06 18:09:23 +0800

    修改实体

--- order-jdbc/src/main/java/com/three/order/orderjdbc/entity/TbCatParam.java
+++ order-jdbc/src/main/java/com/three/order/orderjdbc/entity/TbCatParam.java
@@ -3,6 +3,7 @@ package com.three.order.orderjdbc.entity;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Component
@@ -16,53 +17,8 @@ public class TbCatParam {
   @Column(name = "cat_param")
   private String catParam;
   @Column(name = "create_time")
-  private java.sql.Timestamp createTime;
+  private Date createTime;
   @Column(name = "modi_time")
-  private java.sql.Timestamp modiTime;
-
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public String getCatNo() {
-    return catNo;
-  }
-
-  public void setCatNo(String catNo) {
-    this.catNo = catNo;
-  }
-
-
-  public String getCatParam() {
-    return catParam;
-  }
-
-  public void setCatParam(String catParam) {
-    this.catParam = catParam;
-  }
-
-
-  public java.sql.Timestamp getCreateTime() {
-    return createTime;
-  }
-
-  public void setCreateTime(java.sql.Timestamp createTime) {
-    this.createTime = createTime;
-  }
-
-
-  public java.sql.Timestamp getModiTime() {
-    return modiTime;
-  }
-
-  public void setModiTime(java.sql.Timestamp modiTime) {
-    this.modiTime = modiTime;
-  }
+  private Date modiTime;
 
 }
